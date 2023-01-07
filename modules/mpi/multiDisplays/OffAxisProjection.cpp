a50758bb7db4048cb9a20288e58de7ea0f1ee72e
Jung Who Nam <jnam@tacc.utexas.edu>
2023-01-07 17:59:06 -0600

    Integrate monitor mullion sizes

--- modules/mpi/multiDisplays/OffAxisProjection.cpp
+++ modules/mpi/multiDisplays/OffAxisProjection.cpp
@@ -20,7 +20,7 @@ OffAxisProjection::OffAxisProjection(int mpiRank)
     throw std::runtime_error("Failed to parse display_settings.json!");
   }
 
-  // update camera variables
+   // update three corners of the image plane
   {
     std::vector<float> vals = config[mpiRank]["topLeft"];
     topLeftLocal.x = vals[0];
@@ -42,10 +42,29 @@ OffAxisProjection::OffAxisProjection(int mpiRank)
     botRightLocal.z = vals[2];
     perspective->botRight = botRightLocal;
   }
-  camera.setParam("aspect", 
-    rkcommon::math::length(botRightLocal - botLeftLocal) / 
-    rkcommon::math::length(topLeftLocal - botLeftLocal)
-  );
+  // use mullion values to update the three corners
+  {
+    vec3f tl = topLeftLocal, bl = botLeftLocal, br = botRightLocal;
+
+    float mullionLeft = config[mpiRank]["mullionLeft"];
+    botLeftLocal += normalize(br - bl) * mullionLeft;
+    topLeftLocal +=  normalize(br - bl) * mullionLeft;
+
+    float mullionRight = config[mpiRank]["mullionRight"];
+    botRightLocal += normalize(bl - br) * mullionRight;
+
+    float mullionTop = config[mpiRank]["mullionTop"];
+    topLeftLocal += normalize(bl - tl) * mullionTop;
+
+    float mullionBottom = config[mpiRank]["mullionBottom"];
+    botLeftLocal += normalize(tl - bl) * mullionBottom;
+    botRightLocal += normalize(tl - bl) * mullionBottom;
+  }
+
+  // update aspect ratio and fovy
+  camera.setParam("aspect", length(botRightLocal - botLeftLocal) / length(topLeftLocal - botLeftLocal));
+  float angle = acos(dot(topLeftLocal, botLeftLocal) / (length(topLeftLocal) * length(botLeftLocal))) * 180.f / M_PI;
+  camera.setParam("fovy", angle);
   camera.commit();
 }
 
